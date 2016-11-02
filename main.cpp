5e6930d22e4595a8481e37847c0f1c0cecb6dcf8
KiselViktor <kiselviktor2009@yandex.ru>
2016-11-02 16:20:41 +0300

    corrected errors

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    cout<<"##### This is a program for creating dictionaries. #####"<<endl;
+    cout<<"\t\tThis is a program for creating dictionaries."<<endl;
     string file_name;
     int operator_code;
     cout<<endl<<"Name of file: ";
@@ -38,20 +38,20 @@ int main()
                             {
                                 arr[6] = g;
                                 i++;
-                                if((a!=b||b!=c||c!=d)&& //xxxx012
-                                   (b!=c||c!=d||d!=e)&& //0xxxx12
-                                   (c!=d||d!=e||e!=f)&& //01xxxx2
-                                   (d!=e||e!=f||f!=g)&& //012xxxx
-                                   (b!=(a+1)||c!=(a+2)||d!=(a+3)||e!=(a+4))&&   //x(x+1)(x+2)(x+3)(x+4)01
-                                   (c!=(b+1)||d!=(b+2)||e!=(b+3)||f!=(b+4))&&   //0x(x+1)(x+2)(x+3)(x+4)1
-                                   (d!=(c+1)||e!=(c+2)||f!=(c+3)||g!=(c+4))&&   //01x(x+1)(x+2)(x+3)(x+4)
-                                   (f!=(g-1)||e!=(g-2)||d!=(g-3)||c!=(g-4))&&   //01(x-4)(x-3)(x-2)(x-1)x
-                                   (e!=(f-1)||d!=(f-2)||c!=(f-3)||b!=(f-4))&&   //0(x-4)(x-3)(x-2)(x-1)x1
-                                   (d!=(e-1)||c!=(e-2)||b!=(e-3)||a!=(e-4))&&   //(x-4)(x-3)(x-2)(x-1)x01
-                                   (a!=b||b!=c||e!=f||f!=g)&&   // xxx0yyy
-                                   (b!=c||c!=d||e!=f||f!=g)&&   // 0xxxyyy
-                                   (a!=b||b!=c||d!=e||e!=f)   // xxxyyy0
-                                   //3 пары по 2
+                                if((a!=b||b!=c||c!=d)&&                             //xxxx012
+                                   (b!=c||c!=d||d!=e)&&                             //0xxxx12
+                                   (c!=d||d!=e||e!=f)&&                             //01xxxx2
+                                   (d!=e||e!=f||f!=g)&&                             //012xxxx
+                                   (b!=(a+1)||c!=(a+2)||d!=(a+3)||e!=(a+4))&&       //x(x+1)(x+2)(x+3)(x+4)01
+                                   (c!=(b+1)||d!=(b+2)||e!=(b+3)||f!=(b+4))&&       //0x(x+1)(x+2)(x+3)(x+4)1
+                                   (d!=(c+1)||e!=(c+2)||f!=(c+3)||g!=(c+4))&&       //01x(x+1)(x+2)(x+3)(x+4)
+                                   (f!=(g+1)||e!=(g+2)||d!=(g+3)||c!=(g+4))&&       //01(x+4)(x+3)(x+2)(x+1)x
+                                   (e!=(f+1)||d!=(f+2)||c!=(f+3)||b!=(f+4))&&       //0(x+4)(x+3)(x+2)(x+1)x1
+                                   (d!=(e+1)||c!=(e+2)||b!=(e+3)||a!=(e+4))&&       //(x+4)(x+3)(x+2)(x+1)x01
+                                   (a!=b||b!=c||e!=f||f!=g)&&                       // xxx0yyy
+                                   (b!=c||c!=d||e!=f||f!=g)&&                       // 0xxxyyy
+                                   (a!=b||b!=c||d!=e||e!=f)                         // xxxyyy0
+                                    //3 пары по 2
                                    )
                                 {
                                     password<<operator_code;
